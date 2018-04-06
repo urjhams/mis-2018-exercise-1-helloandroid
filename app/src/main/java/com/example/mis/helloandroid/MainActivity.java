48d54d5d4c45f2392bb4634f4ccb30db1ec5439c
1und0 <andreasberst@googlemail.com>
2018-04-06 10:12:27 +0200

    Common ground example

---
+++ app/src/main/java/com/example/mis/helloandroid/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.mis.helloandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void helloWorld(View view) {
+        Toast toast = Toast.makeText(MainActivity.this, "hey there", Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER, 0 ,0);
+        toast.show();
+    }
+}
